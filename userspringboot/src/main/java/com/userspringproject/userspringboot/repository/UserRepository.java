3e2b54b8fd7d392786fcadc133b85d7c09aeb01e
Sufyan Azar <66428774+sufyanazar786@users.noreply.github.com>
2024-09-10 11:00:11 +0530

    Add files via upload

---
+++ userspringboot/src/main/java/com/userspringproject/userspringboot/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.userspringproject.userspringboot.repository;
+
+import com.userspringproject.userspringboot.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+}
